293447f8900836fa3b78e400ba78685d2a69b8ae
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-17 13:04:59 +0200

    Added basic pygame main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+pygame.event.set_allowed([QUIT, KEYDOWN, MOUSEBUTTONDOWN])  # If you need event to be handled - add it to the list.
+# Only add these events, which are needed to save performance
+
+# Screen initialisation
+SCREEN = pygame.display.set_mode((settings["width"], settings["height"]), flags, 1)
+SCREEN.set_alpha(None)
+pygame.display.set_caption("Pong")
