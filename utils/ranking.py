99e9bc64a9f306ed6bf317298ece2b64e399bd4d
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-17 14:34:25 +0200

    Fixed addHighscore Authored-by: Maciek

--- utils/ranking.py
+++ utils/ranking.py
@@ -1,14 +1,23 @@
 from utils.jsonHandler import readJSON, writeJSON
 
 
-def readRanking():
-    return readJSON("./ranking.json")
-
 
 def writeRanking(toWrite):
     writeJSON("./ranking.json", toWrite)
 
+def readRanking():
+    return readJSON("./ranking.json")
+
+def rankingLenghtCheck():
+    ranking = readRanking()
+    if len(ranking) < 10:
+        for i in range(len(ranking)+1,11,1):
+            nullscore = {"initials": "NUL", "score": 000}
+            ranking[str(i)] = nullscore
+        writeRanking(ranking)
+
 def addHighScore(initials, score):
+    rankingLenghtCheck()
     ranking = readRanking()
     for i in ranking:
         if score > ranking[i]["score"]:
