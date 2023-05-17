186ab11421e337dca434068e282ea0a1443c4e2b
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-17 14:10:45 +0200

    Added addHighScore function, which writes to JSON Authored-by: Maciek

--- utils/ranking.py
+++ utils/ranking.py
@@ -11,4 +11,13 @@ def writeRanking(toWrite):
     writeJSON("./ranking.json", toWrite)
 
 def addHighScore(initials, score):
-    
+    ranking = readRanking()
+    for i in ranking:
+        if score > ranking[i]["score"]:
+            tmpinitials = ranking[i]["initials"]
+            tmpscore = ranking[i]["score"]
+            ranking[i]["initials"] = initials
+            ranking[i]["score"] = score
+            initials = tmpinitials
+            score = tmpscore
+    writeRanking(ranking)
