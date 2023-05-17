8a1e024ad08756072ccc382023f4f11bebaf46b6
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-17 15:12:55 +0200

    Changed ranking.json to rankingSingle.json Added rankingDouble.json Authored-by: Maciek Tested-by: WiktorGórecki <wiktor.gorecki@o2.pl>

--- utils/ranking.py
+++ utils/ranking.py
@@ -28,3 +28,36 @@ def addHighScoreSingle(initials, score):
             initials = tmpinitials
             score = tmpscore
     writeRankingSingle(ranking)
+
+######################################################
+
+def writeRankingDouble(toWrite):
+    writeJSON("./rankingDouble.json", toWrite)
+
+def readRankingDouble():
+    return readJSON("./rankingDouble.json")
+
+def rankingDoubleLenghtCheck():
+    ranking = readRankingDouble()
+    if len(ranking) < 10:
+        for i in range(len(ranking)+1,11,1):
+            nullscore = {"initials1": "NUL", "initials2": "NUL", "score": 000}
+            ranking[str(i)] = nullscore
+        writeRankingDouble(ranking)
+
+def addHighScoreDouble(initials1, initials2, score):
+    print("Debug: Adding highscore for doubleplayer")
+    rankingDoubleLenghtCheck()
+    ranking = readRankingDouble()
+    for i in ranking:
+        if score > ranking[i]["score"]:
+            tmpinitials1 = ranking[i]["initials1"]
+            tmpinitials2 = ranking[i]["initials2"]
+            tmpscore = ranking[i]["score"]
+            ranking[i]["initials1"] = initials1
+            ranking[i]["initials2"] = initials2
+            ranking[i]["score"] = score
+            initials1 = tmpinitials1
+            initials2 = tmpinitials2
+            score = tmpscore
+    writeRankingDouble(ranking)
