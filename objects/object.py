422bacabb50999cfffe2f82dbd629e38e2d47553
malgos208 <malgo.stypa@gmail.com>
2023-05-17 14:57:27 +0200

    game object wall detection

--- objects/object.py
+++ objects/object.py
@@ -1,5 +1,6 @@
 import pygame.draw
 from pygame.math import Vector2
+from utils.settings import readSettings
 
 class GameObject:
     def __init__(self, position, velocity):
@@ -8,12 +9,27 @@ class GameObject:
         self.velocity = Vector2(velocity)
         self.height = 40
         self.width = 40
+        self.settings = readSettings()
 
     def draw(self, surface):
         pass
 
     def move(self):
         pass
+
+    def handleCollisionLeft(self):
+        return self.position[0] <= 0
+
+    def handleCollisionRight(self):
+        width = self.settings['width'];
+        return self.self.position[0]+self.width >= width
+
+    def handleCollisionDown(self):
+        height = self.settings['height'];
+        return self.self.position[1] + self.width >= height
+
+    def handleCollisionTop(self):
+        return self.position[1] <= 0
 
     # def collides_with(self, other_obj):
     #     distance = self.position.distance_to(other_obj.position)
