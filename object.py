055298f075e4c614d5b54cfdd996da59b878eb07
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-17 13:29:35 +0200

    Imported object.py

---
+++ object.py
@@ -0,0 +1,21 @@
+import pygame.draw
+from pygame.math import Vector2
+
+class GameObject:
+    def __init__(self, position, velocity):
+        self.position = Vector2(position)
+        self.velocity = Vector2(velocity)
+        self.height = 40
+        self.width = 40
+
+    def draw(self, surface):
+        # blit_position = self.position - Vector2(self.radius)
+        # surface.blit(self.sprite, blit_position)
+        pygame.draw.rect(surface, "green", pygame.Rect(0,0, self.height, self.width))
+
+    def move(self):
+        self.position = self.position + self.velocity
+
+    # def collides_with(self, other_obj):
+    #     distance = self.position.distance_to(other_obj.position)
+    #     return distance < self.radius + other_obj.radius
