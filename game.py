3b7c639449460be962fdbab4a857c89adbaa252a
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-18 14:37:45 +0200

    Added hearts and score

--- game.py
+++ game.py
@@ -24,11 +24,13 @@ from utils.stats import stats
 
 class Game:
     def __init__(self):
+        print("Debug: Resetting stats")
+        stats['score'] = 0
+        stats['healthpoints'] = 3
         shipImg = pygame.image.load("assets/ship.svg")
         shipImg2 = pygame.image.load("assets/ship.svg")
         asteroidImg = pygame.image.load("assets/asteroid2.svg")
         self.settings = readSettings()
-        self._init_pygame()
         self.screen = pygame.display.set_mode((1280, 720))
         self.spaceship = Spaceship((400, 300), (0.2,0.2), shipImg)
         self.keyPressed = []
@@ -53,9 +55,6 @@ class Game:
 
 
 
-    def _init_pygame(self):
-        pygame.init()
-        pygame.display.set_caption("ASTEROIDS")
 
     def _handle_input(self, keys):
         for event in pygame.event.get():
@@ -82,7 +81,7 @@ class Game:
             pygame.draw.rect(self.screen, "blue", pygame.Rect(bulletPosition, (5, 5)))
             # print(self.spaceship.angle)
             # self.bullets.append(Bullet(bulletPosition, (self.spaceship.velocity).rotate(self.spaceship.angle), self.spaceship.angle))
-            self.bullets.append(Bullet(bulletPosition, (self.spaceship.velocity).rotate(self.spaceship.angle), self.spaceship.angle))
+            self.bullets.append(Bullet(bulletPosition, (self.spaceship.velocity*5).rotate(self.spaceship.angle), self.spaceship.angle))
 
     def _process_game_logic(self):
         self.spaceship.move(self.keyPressed)
@@ -125,6 +124,24 @@ class Game:
 
     def _draw(self):
         self.screen.fill("black")
+        SCORE_TEXT = get_font(25).render("SCORE:", True, "green")
+        SCORE_RECT = SCORE_TEXT.get_rect(center=(50, 50))
+        self.screen.blit(SCORE_TEXT, SCORE_RECT)
+
+        NUMBER_TEXT = get_font(25).render(str(stats['score']), True, "green")
+        NUMBER_RECT = NUMBER_TEXT.get_rect(center=(150, 50))
+        self.screen.blit(NUMBER_TEXT, NUMBER_RECT)
+
+        heart = pygame.image.load('assets/heart.svg')
+        for i in range(0, stats['healthpoints']):
+            HEART_ICON = pygame.transform.scale(heart, (25, 25))
+            HEART_RECT = HEART_ICON.get_rect(center=(50 + i * 25, 100))
+            self.screen.blit(HEART_ICON, HEART_RECT)
+
+        if stats['healthpoints'] <= 0:
+            from screens.rankingAddSingle import rankingAddSingle
+            rankingAddSingle(self.screen, stats['score'])
+
         self.spaceship.draw(self.screen)
         for asteroid in self.asteroids:
             asteroid.draw(self.screen)
