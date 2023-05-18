ecea683a07c646a0398a84acf6af367ecead1642
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-18 09:37:40 +0200

    Authored-by: Łukasz

--- objects/laser.py
+++ objects/laser.py
@@ -2,7 +2,7 @@ import pygame
 
 from objects.object import GameObject
 
-class Lazer (GameObject):
+class Laser (GameObject):
     def __int__(self, position, velocity):
         print("Debug: Creating new Laser object")
         GameObject.__init__(self, position, velocity)
