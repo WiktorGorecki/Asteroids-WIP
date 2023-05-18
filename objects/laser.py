db93ffb4709442c291293a260b9931d5a4d0f40d
WiktorGórecki <wiktor.gorecki@o2.pl>
2023-05-18 12:16:08 +0200

    Changes by Łukasz Ja do tego ręki nie przyłożyłem i nie dam sobie niej za ten fragment obciąć, bo pisał to Łukasz Authored-by: Łukasz Tested-by: Nobody :[

--- objects/laser.py
+++ objects/laser.py
@@ -6,5 +6,3 @@ class Laser (GameObject):
     def __int__(self, position, velocity):
         print("Debug: Creating new Laser object")
         GameObject.__init__(self, position, velocity)
-        self.width = 5
-        self.height = 5
